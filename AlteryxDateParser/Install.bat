0d5815a47996f94c7a1b82916882f0ec65baceeb
James Dunkerley <james.dunkerley@gmail.com>
2016-03-01 16:22:14 +0000

    Different registry key to find Alteryx

--- AlteryxDateParser/Install.bat
+++ AlteryxDateParser/Install.bat
@@ -10,34 +10,34 @@ if '%errorlevel%' NEQ '0' (
         exit /B -1
     )
 ) else (
-	goto gotAdmin
+    goto gotAdmin
 )
 
 :UACPrompt
-	echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-	echo UAC.ShellExecute "%~s0", "ELEV", "", "runas", 1 >> "%temp%\getadmin.vbs"
-	"%temp%\getadmin.vbs"
-	exit /B
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "ELEV", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
 
 :gotAdmin
-	FOR /F "usebackq tokens=2,* skip=2" %%L IN (
-		`reg query "HKCU\SOFTWARE\SRC\Alteryx" /v LastInstallDir`
-	) DO SET alteryxPath=%%M
+    FOR /F "usebackq tokens=2,* skip=2" %%L IN (
+        `reg query "HKLM\SOFTWARE\Wow6432Node\SRC\Alteryx" /v InstallDir64`
+    ) DO SET alteryxPath=%%M
 
-	pushd "%~dp0"
+    pushd "%~dp0"
 
-	echo [Settings] > "JDTools.ini"
-	echo x64Path=%cd% >> "JDTools.ini"
-	echo x86Path=%cd% >> "JDTools.ini"
-	echo ToolGroup=JDTools >> "JDTools.ini"
+    echo [Settings] > "JDTools.ini"
+    echo x64Path=%cd% >> "JDTools.ini"
+    echo x86Path=%cd% >> "JDTools.ini"
+    echo ToolGroup=JDTools >> "JDTools.ini"
 
-        if '%alteryxPath%' NEQ '' (
-            xcopy JDTools.ini "%alteryxPath%\..\Settings\AdditionalPlugins\" /Y /Q
-            del JDTools.ini /Q
-	    echo Config installed to "%alteryxPath%\..\Settings\AdditionalPlugins\JDTools.ini"
-        ) else (
-            echo Please copy "%cd$\JDTools.ini" to <AlteryxInstallDir>\Settings\AdditionalPlugins
-        )
-        
-	popd
-	pause
+    if '%alteryxPath%' NEQ '' (
+        xcopy JDTools.ini "%alteryxPath%\..\Settings\AdditionalPlugins\" /Y /Q
+        del JDTools.ini /Q
+    echo Config installed to "%alteryxPath%\..\Settings\AdditionalPlugins\JDTools.ini"
+    ) else (
+        echo Please copy "%cd$\JDTools.ini" to <AlteryxInstallDir>\Settings\AdditionalPlugins
+    )
+
+    popd
+    pause
--- AlteryxDateParser/Uninstall.bat
+++ AlteryxDateParser/Uninstall.bat
@@ -3,26 +3,31 @@
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
 if '%errorlevel%' NEQ '0' (
-	echo Requesting administrative privileges...
-	goto UACPrompt
+    echo Requesting administrative privileges...
+    goto UACPrompt
 ) else (
-	goto gotAdmin
+    goto gotAdmin
 )
 
 :UACPrompt
-	echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-	echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-	"%temp%\getadmin.vbs"
-	exit /B
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
 
 :gotAdmin
-	FOR /F "usebackq tokens=2,* skip=2" %%L IN (
-		`reg query "HKCU\SOFTWARE\SRC\Alteryx" /v LastInstallDir`
-	) DO SET alteryxPath=%%M
+    FOR /F "usebackq tokens=2,* skip=2" %%L IN (
+        `reg query "HKLM\SOFTWARE\Wow6432Node\SRC\Alteryx" /v InstallDir64`
+    ) DO SET alteryxPath=%%M
 
-	pushd "%alteryxPath%\..\Settings\AdditionalPlugins\"
-    del JDTools.ini /Q
-	popd
+    if '%alteryxPath%' NEQ '' (
+        pushd "%alteryxPath%\..\Settings\AdditionalPlugins\"
+        del JDTools.ini /Q
+        echo Deleted installed config from "%alteryxPath%\..\Settings\AdditionalPlugins\JDTools.ini"
+    echo Config installed to "%alteryxPath%\..\Settings\AdditionalPlugins\JDTools.ini"
+    ) else (
+        echo Please delete "%cd$\JDTools.ini" from <AlteryxInstallDir>\Settings\AdditionalPlugins
+    )
 
-	echo Deleted installed config from "%alteryxPath%\..\Settings\AdditionalPlugins\JDTools.ini"
-	pause
+    popd
+    pause
