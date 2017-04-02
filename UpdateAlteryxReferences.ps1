172a637b0ed595c9035efa636b354adfcddc21a7
James Dunkerley <james.dunkerley@gmail.com>
2017-04-02 21:46:52 +0100

    Script to update Alteryx Versions

---
+++ UpdateAlteryxReferences.ps1
@@ -0,0 +1,23 @@
+﻿$root = Split-Path -Parent $PSCommandPath
+Write-Host "Finding Alteryx Install Location..."
+$reg = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\SRC\Alteryx -ErrorAction SilentlyContinue
+if ($reg -eq $null) {
+    $reg = Get-ItemProperty HKCU:\SOFTWARE\SRC\Alteryx -ErrorAction SilentlyContinue
+}
+
+if ($reg -eq $null) {
+    Write-Host "Couldn't Find Alteryx"
+    exit -1
+}
+
+Write-Host "Found " $reg.InstallDir64
+$dir = $reg.InstallDir64
+
+Push-Location $root
+
+Get-ChildItem -File -Filter *.csproj -Recurse  | ForEach {
+     (Get-Content $_.FullName ).Replace("<HintPath>.*?\\(AlteryxGuiToolkit\.dll|AlteryxRecordInfo\.Net\.dll)</HintPath>", '<HintPath>$dir\$1</HintPath>') | 
+     Set-Content $_.FullName
+}
+
+Pop-Location
