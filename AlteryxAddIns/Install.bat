01f7c2cbac5d66de8f9246bea9a2e371a0f27192
James Dunkerley <james.dunkerley@gmail.com>
2017-04-16 21:45:32 +0100

    Clean up development installers

--- AlteryxAddIns/Install.bat
+++ AlteryxAddIns/Install.bat
@@ -1,2 +1,4 @@
 @echo off
 powershell "Start-Process -FilePath powershell.exe -ArgumentList '%~fs0\..\Scripts\Installer.ps1', 'OmniBus', 'OmniBus' -verb RunAs"
+@echo off
+powershell "Start-Process -FilePath powershell.exe -ArgumentList '%~fs0\..\Scripts\Installer.ps1', 'OmniBus', 'OmniBus' -verb RunAs"
